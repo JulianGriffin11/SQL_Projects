070ab439d50c7024e0acb250c32fe2ad3ce496e1
JulianGriffin11 <juliangriffinj@gmail.com>
2024-12-30 00:17:53 -0500

    Update 1_top_paying_jobs.sql

--- Project 1 - Job Jury/project_sql/1_top_paying_jobs.sql
+++ Project 1 - Job Jury/project_sql/1_top_paying_jobs.sql
@@ -3,14 +3,15 @@ Question: What are the top paying data analyst jobs?
 */
 
 SELECT
-    job_id,
-    job_title,
-    job_location,
-    job_country,
-    job_schedule_type,
-    salary_year_avg,
-    job_posted_date:: DATE AS job_posted_date,
-    name AS company_name
+    job_id AS ID,
+    job_title AS Title,
+    job_location AS Location,
+    job_country AS Country,
+    job_schedule_type AS Schedule,
+    ROUND(salary_year_avg, 0) AS Average_Salary,
+    job_posted_date:: DATE AS Posted_Date,
+    CURRENT_DATE - job_posted_date:: DATE AS Posting_Age_in_Days, -- calculates how old the job posting is
+    name AS Company_Name
 FROM
     job_postings_fact
 
@@ -18,7 +19,7 @@ LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
 
 WHERE
     job_title_short = 'Data Analyst' AND
-    job_location = 'Anywhere' AND
+    job_country IN ('Canada', 'United States') AND
     salary_year_avg IS NOT NULL
 ORDER BY
     salary_year_avg DESC
